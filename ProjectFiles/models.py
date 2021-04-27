07c6a3062da914b30137fab12ff66472f69d1e18
Jalen Young <jyoun138@uncc.edu>
2021-04-27 15:56:27 -0400

    fixed error in models

--- ProjectFiles/models.py
+++ ProjectFiles/models.py
@@ -40,7 +40,7 @@ class Friends(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     user_id = db.Column("user_id", db.Integer, db.ForeignKey('user.id'))
     user_id_2 = db.Column("user_id_2", db.Integer)
-    friendUsername = db.Column("username", db.String(100), db.ForeignKey('user.username'))
+    friendUsername = db.Column("username", db.String(100))
 
     def __init__(self, user_id, user_id_2, friendUsername):
         self.user_id = user_id
